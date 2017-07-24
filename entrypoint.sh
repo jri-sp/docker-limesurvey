eab090e17fe2e0faa52e6b541e3bd83d0d52dea1
jri-sp <jeremy.richard@sciences-po.fr>
2017-07-24 16:39:04 +0200

    Hide Apache & PHP version

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,15 @@ then
   sed -i.original -e '/^<VirtualHost[ ]\*:80>/,/<\/VirtualHost>/ { /<\/VirtualHost>/ i\Alias '"$BASE_URI"' /var/www/html' -e '}' /etc/apache2/sites-enabled/000-default.conf
 fi
 
+# Hide PHP version
+echo "expose_php=off" >> /usr/local/etc/php/conf.d/security.ini
+
 # Apache things
+# Hide version
+sed -i "s/^ServerTokens OS$/ServerTokens Prod/" /etc/apache2/conf-available/security.conf
+sed -i "s/^ServerSignature On$/ServerSignature Off/" /etc/apache2/conf-available/security.conf
+echo "UseCanonicalName On" >> /etc/apache2/conf-available/security.conf
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- apache2-foreground "$@"
